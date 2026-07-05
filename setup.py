fe365069d55990d8827408323d6b7ac3c8f0ef19
Athira PT <athirapt998@gmail.com>
2026-07-05 21:04:13 +0530

    add local package setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgen',
+    version='0.0.1',
+    author='athira',
+    author_email='athirapt998@gmail.com',
+    install_requires=[
+    "langchain==1.2.13",
+    "langchain-community==0.4.1",
+    "langchain-core==1.2.22",
+    "langchain-groq==1.1.2",
+    "streamlit",
+    "python-dotenv",
+    "PyPDF2"
+],
+    packages=find_packages()
+)
