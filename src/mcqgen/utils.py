4a6ef4d05443f2cc5db093094eed055fc8b42314
Athira PT <athirapt998@gmail.com>
2026-07-08 20:02:18 +0530

    add helper functions

---
+++ src/mcqgen/utils.py
@@ -0,0 +1,46 @@
+import os
+# pyrefly: ignore [missing-import]
+import PyPDF2
+import json
+import traceback
+
+
+def read_file(file):
+    if file.name.endswith(".pdf"):
+        try:
+            pdf_reader = PyPDF2.PdfReader(file)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            return text
+        except Exception as e:
+            raise Exception(f"Error reading PDF file: {e}")
+    
+    elif file.name.endswith(".txt"):
+        try:
+            return file.read().decode("utf-8")
+        except Exception as e:
+            raise Exception(f"Error reading TXT file: {e}")
+    
+    else:
+        raise Exception(f"Error reading file: {file.name}. Only pdf and txt files are suppported")
+
+def get_table_data(quiz_str):
+    try:
+        quiz_dict = json.loads(quiz_str)
+        quiz_table_data = []
+        for key, value in quiz_dict.items():
+            mcq = value["mcq"]
+            options = " | ".join(
+                [
+                    f"{option}: {option_value}"
+                    for option, option_value in value["options"].items()
+                    ]
+                )
+            correct = value["correct"]
+            quiz_table_data.append({"MCQ": mcq, "Choices": options, "Correct": correct})
+        return quiz_table_data
+    
+    except Exception as e:
+        traceback.print_exception(e)
+        return False
